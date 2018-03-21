f8974adbe5b5787d0aa44cfb8ba95d3eba613132
kobanyan <kobanyan@users.noreply.github.com>
2018-03-21 14:23:38 +0900

    Fix reducer test

--- src/options/__tests__/module.test.ts
+++ src/options/__tests__/module.test.ts
@@ -1,20 +1,13 @@
 import { createStore } from 'redux';
 import * as module from '../module';
 
-let store;
-
 describe('options module', () => {
-  beforeEach(() => {
-    store = createStore(module.reducer);
-  });
   it('should update favoriteColor only', () => {
-    expect(store.getState()).toEqual(module.INITIAL_STATE);
-    store.dispatch(module.updateFavoriteColor('yellow'));
-    expect(store.getState()).toEqual({ favoriteColor: 'yellow', likesColor: false });
+    const state = module.reducer(module.INITIAL_STATE, module.updateFavoriteColor('yellow'));
+    expect(state).toEqual({ favoriteColor: 'yellow', likesColor: false });
   });
   it('should update likesColor only', () => {
-    expect(store.getState()).toEqual(module.INITIAL_STATE);
-    store.dispatch(module.updateLikesColor(true));
-    expect(store.getState()).toEqual({ favoriteColor: 'red', likesColor: true });
+    const state = module.reducer(module.INITIAL_STATE, module.updateLikesColor(true));
+    expect(state).toEqual({ favoriteColor: 'red', likesColor: true });
   });
 });
